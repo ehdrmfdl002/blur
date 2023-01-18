d8b6535774ce90107ec57aac92f9c0574c4b4821
김창겸 <kyum8562@naver.com>
2023-01-18 17:56:27 +0900

    :art: HomePage modify and meetingpage ing
    HomePage modify and meetingPage ing

--- FE/src/pages/Meeting/index.jsx
+++ FE/src/pages/Meeting/index.jsx
@@ -7,13 +7,20 @@ function Meeting() {
       <Header />
       <div className="MCenterDiv">
         <div className="MCenterImgDiv"></div>
-        <div className="MCenterTitle"></div>
-        <div className="MCenterDesc"></div>
+        <div className="MCenterTitle">Searching Other :)</div>
+        <div className="MCenterDesc">Please wait For 5 minutes.</div>
         <div className="MCenterCirclesDiv">
           <div className="MPCenterCircle1"></div>
           <div className="MPCenterCircle2"></div>
           <div className="MPCenterCircle3"></div>
         </div>
+      </div>
+      <div className="MLeftDiv">
+        <div className="MMyCamLabel">My Camera</div>
+        <div className="MMyCamDiv"></div>
+      </div>
+      <div className="MStopBtnDiv">
+        <span className="MStopBtnText">Stop</span>
       </div>
     </div>
   );

8c1f07dc3a7ad0719c8a59f5c24f93c1e8b3ab69
kimseonghun96 <tjd951753@naver.com>
2023-01-19 14:55:32 +0900

    Merge branch 'fe' of https://lab.ssafy.com/s08-webmobile1-sub2/S08P12B307 into fe

--- FE/src/App.js
+++ FE/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 
 import "./App.css";
 
+import Start from "./pages/Start";
 import Meeting from "./pages/Meeting";
 import MyInfo from "./pages/MyInfo";
 
@@ -11,7 +12,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Start />}></Route>
           <Route path="/meeting" element={<Meeting />}></Route>
           <Route path="/MyInfo" element={<MyInfo />}></Route>
         </Routes>
